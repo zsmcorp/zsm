5fff33af2a4ebd86201b515bb2c33e667352d9ca
maeiiei <maks.markin.05@mail.ru>
2022-11-23 18:53:11 +0300

    возможно говно

--- zsm/urls.py
+++ zsm/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('timetable/', include('timetables.urls'))
+    path('timetable/', include('timetables.urls')),
+    path('', include('homepage.urls')),
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
