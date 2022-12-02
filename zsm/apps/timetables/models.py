160dcf01c4a3444745362f720417e334e8b64080
maeiiei <maks.markin.05@mail.ru>
2022-12-02 20:16:46 +0300

    AdminUpdate 0.1

--- zsm/apps/timetables/models.py
+++ zsm/apps/timetables/models.py
@@ -5,33 +5,39 @@ class Category(models.Model):
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
     def __str__(self):
         return self.name
-    class Meta
+    class Meta: 
+        verbose_name = 'Класс'
+        verbose_name_plural = 'Классы'
 
 class Timetable(models.Model):
     class_init = models.ForeignKey(Category, on_delete = models.CASCADE, verbose_name = 'Класс')
 
-    first_lesson = models.CharField('first lesson', max_length = 15, default='—', null=True, blank=True)
-    first_cabinet = models.CharField('first cabinet', max_length = 10, default='—', null = True, blank = True)
+    first_lesson = models.CharField('Первый урок', max_length = 15, default='—', null=True, blank=True)
+    first_cabinet = models.CharField('Первый кабинет', max_length = 10, default='—', null = True, blank = True)
 
-    second_lesson = models.CharField('second lesson', max_length = 15, default='—', null=True, blank=True)
-    second_cabinet = models.CharField('second cabinet', max_length = 10, default='—', null = True, blank = True)
+    second_lesson = models.CharField('Второй урок', max_length = 15, default='—', null=True, blank=True)
+    second_cabinet = models.CharField('Второй кабинет', max_length = 10, default='—', null = True, blank = True)
 
-    third_lesson = models.CharField('third lesson', max_length = 15, default='—', null=True, blank=True)
-    third_cabinet = models.CharField('third cabinet', max_length = 10, default='—', null = True, blank = True)
+    third_lesson = models.CharField('Третий урок', max_length = 15, default='—', null=True, blank=True)
+    third_cabinet = models.CharField('Третий кабинет', max_length = 10, default='—', null = True, blank = True)
 
-    fourth_lesson = models.CharField('fourth lesson', max_length = 15, default='—', null=True, blank=True)
-    fourth_cabinet = models.CharField('fourth cabinet', max_length = 10, default='—', null = True, blank = True)
+    fourth_lesson = models.CharField('Четвёртый урок', max_length = 15, default='—', null=True, blank=True)
+    fourth_cabinet = models.CharField('Четвёртый кабинет', max_length = 10, default='—', null = True, blank = True)
 
-    fifth_lesson = models.CharField('fifth lesson', max_length = 15, default='—', null=True, blank=True)
-    fifth_cabinet = models.CharField('fifth cabinet', max_length = 10, default='—', null = True, blank = True)
+    fifth_lesson = models.CharField('Пятый урок', max_length = 15, default='—', null=True, blank=True)
+    fifth_cabinet = models.CharField('Пятый кабинет', max_length = 10, default='—', null = True, blank = True)
 
-    sixth_lesson = models.CharField('sixth lesson', max_length = 15, default='—', null=True, blank=True)
-    sixth_cabinet = models.CharField('sixth cabinet', max_length = 10, default='—', null = True, blank = True)
+    sixth_lesson = models.CharField('Шестой урок', max_length = 15, default='—', null=True, blank=True)
+    sixth_cabinet = models.CharField('Шестой кабинет', max_length = 10, default='—', null = True, blank = True)
 
-    seventh_lesson = models.CharField('seventh lesson', max_length = 15, default='—', null=True, blank=True)
-    seventh_cabinet = models.CharField('second cabinet', max_length = 10, default='—', null = True, blank = True)
+    seventh_lesson = models.CharField('Седьмой урок', max_length = 15, default='—', null=True, blank=True)
+    seventh_cabinet = models.CharField('Седьмой кабинет', max_length = 10, default='—', null = True, blank = True)
 
-    timetable_date = models.DateField('timetable date')
+    timetable_date = models.DateField('Дата расписания')
 
-    short_lesson = models.BooleanField('short lesson')
-    short_break = models.BooleanField('short break')
+    short_lesson = models.BooleanField('Короткие уроки')
+    short_break = models.BooleanField('Короткие перемены')
+
+    class Meta: 
+        verbose_name = 'Расписание'
+        verbose_name_plural = 'Расписания'
