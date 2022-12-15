43cad2a8eb322d0e3663034afaa0bbf848949007
zotkinsb <zotkinsb@icloud.com>
2022-12-15 17:04:43 +0300

    Commit for eblan

--- zsm/apps/timetables/views.py
+++ zsm/apps/timetables/views.py
@@ -3,10 +3,10 @@ from django.http import Http404, HttpResponseRedirect
 from .models import Timetable, Category
 
 
-
 def index(request):
     class_list = Category.objects.all()
-    return render(request, 'timetables/list.html', {'class_list':class_list})
+    return render(request, 'timetables/list.html', {'class_list': class_list})
+
 
 # def detail(request, timetable_slug):
 #     class_id = Category.objects.filter(slug=timetable_slug)
@@ -15,12 +15,11 @@ def index(request):
 
 def detail(request, tt_slug):
     try:
-        class_id = Category.objects.get(slug = tt_slug)
+        class_id = Category.objects.get(slug=tt_slug)
     except:
         raise Http404("Class not found!")
     # class_id = get_object_or_404(Category, slug = timetable_slug)
     # class_id = Category.objects.get(slug=timetable_slug)
     timetables_list = Timetable.objects.filter(class_init_id=class_id.id)
 
-    return render(request, 'timetables/detail.html', {'timetables_list':timetables_list,
-    'class_id':class_id})
+    return render(request, 'timetables/detail.html', {'timetables_list': timetables_list, 'class_id': class_id})
